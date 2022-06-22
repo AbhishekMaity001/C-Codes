5d223c90b3e9465bcfb99e28761528eeafa12d78
Abhishek Maity <abhishek.maity97@gmail.com>
2022-06-23 01:56:33 +0530

    sample test code

---
+++ first.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+// #include<stdio>
+using namespace std;
+// g++ first.cpp -o first_executable_file // to create an executable file through cmd
+int main()
+{
+    cout << "Hello duniya!!" << endl;
+    return 0;
+}
