52cc236b3c4fbf843a4dd9f7cdadc233578f9af8
Abhishek Maity <abhishek.maity97@gmail.com>
2022-07-06 00:23:52 +0530

    converting decimal to binary!

---
+++ 10-LeetCode-03.cpp
@@ -0,0 +1,23 @@
+// Reverse Integer
+// Compliment of Base 10 int
+// Number compliment
+// Binary to Decimal
+
+#include <iostream>
+#include<math.h>
+using namespace std;
+int main(){
+    int n, i=0;
+    cout << "Enter n : ";
+    cin >> n;
+    int ans=1;
+
+    while (n != 0){
+        int bit = n & 1;
+        ans = (bit * pow(10, i)) + ans;
+        
+        n = n >> 1;
+        i++;
+    }
+    cout<<"Answer is : " << ans << endl;
+}
